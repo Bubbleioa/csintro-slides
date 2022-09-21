94a324d6e7cdccce2acd37dc8de0e3223d52a6f2
Bubbleioa <1342130847@qq.com>
2022-09-21 18:10:03 +0800

    sector2: add game of life

---
+++ sector2/assets/DrawingControls.0f1e641d.js
@@ -0,0 +1,1 @@
+import"./DrawingControls.vue_vue_type_script_setup_true_lang.4abe38e4.js";import{_ as t}from"./DrawingControls.vue_vue_type_script_setup_true_lang.4abe38e4.js";import"./index.95d303b3.js";export{t as default};
---
+++ sector2/assets/DrawingPreview.381080f8.js
@@ -0,0 +1,1 @@
+import"./index.95d303b3.js";import{ai as o}from"./index.95d303b3.js";export{o as default};
